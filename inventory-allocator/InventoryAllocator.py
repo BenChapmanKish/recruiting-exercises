fa49a88c541ee2acf64a9d6423b96c8d0e131c84
Ben Chapman-Kish <ben.chapmankish@gmail.com>
2020-06-10 14:37:33 -0400

    Update comments, add more test cases, and add flag `_SHIP_PARTIAL_ORDERS`

--- inventory-allocator/InventoryAllocator.py
+++ inventory-allocator/InventoryAllocator.py
@@ -1,62 +1,91 @@
 #!/usr/bin/env python3
-# Ben Chapman-Kish June 2020
+# Ben Chapman-Kish - June 2020
 # Deliverr recruiting exercise: inventory allocator
 # https://github.com/deliverr/recruiting-exercises/tree/master/inventory-allocator
 
+from collections import defaultdict
+
 """
-Inventory Allocator static class.
-Contains one static method ship(), which fulfills the requirements of the exercise.
+Inventory Allocator static class
+Contains the class method ship(), which fulfills the requirements of the exercise.
+
+Note: the exercise description does not specify what behaviour to implement if an order
+can only be partially fulfilled - that is, if some items can be shipped in the requested
+quantity but other items cannot.
+I've included a boolean flag `_SHIP_PARTIAL_ORDERS` which you can change to modify this
+behaviour if you so desire, and its default is False.
 """
 class InventoryAllocator():
-    """
-    Determines the best shipments for the order `order`, given the inventory `inventory`.
-    Inventory may be spread across any number of warehouses, which are given in increasing order of cost.
-    Returns a list of maps with the warehouse name as keys. The values are also maps of items to the
-    amount being shipped from this warehouse.
-    """
-    def ship(order, inventory):
-        item_distribution = {}
-        
-        # Iterate over every item and its requested quantity
-        for item, quantity in order.items():
-            remaining = quantity
+    _SHIP_PARTIAL_ORDERS = False
 
-            tentative_item_distribution = {}
+    """
+    Returns a boolean indicating if partial orders should be shipped or not.
+    """
+    @classmethod
+    def shouldShipPartialOrders(cls):
+        return cls._SHIP_PARTIAL_ORDERS
+
+    """
+    Determines the best shipment for the order `order`, given a list of inventories
+    `warehouse_inventories` representing various warehouses.
+    Item inventory may be spread across any number of warehouses, which are provided in
+    increasing order of cost.
+    Returns a list of maps with the warehouse name as keys. The values are also maps of
+    items to the amount being shipped from this warehouse.    
+    """
+    @classmethod
+    def ship(cls, order, warehouse_inventories):
+        # Store a map of warehouses to the map of item-quantities they will ship
+        item_distribution = defaultdict( lambda: {} )
+        
+
+        # Iterate over every item-quantity pair in the order
+        for item, quantity_remaining in order.items():
+            # Store a map of warehouses to the quantity of item that they will ship
+            warehouses_for_item = {}
+
 
             # Iterate over warehouses from cheapest to most expensive
-            for wh in inventory:
-                wh_name = wh["name"]
-                wh_inv = wh["inventory"]
+            for warehouse in warehouse_inventories:
+                warehouse_name = warehouse["name"]
+                warehouse_inv = warehouse["inventory"]
 
-                if item not in wh_inv:
+                # Skip this warehouse if it doesn't contain any of `item`
+                if item not in warehouse_inv or warehouse_inv[item] < 1:
                     continue
 
-                wh_stock = wh_inv[item]
 
-                if remaining <= wh_stock:
-                    tentative_item_distribution[wh_name] = remaining
-                    remaining = 0
+                item_stock = warehouse_inv[item]
+
+                # If there is at least enough stock to finish the order with this
+                # warehouse, break out of the loop
+                if quantity_remaining <= item_stock:
+                    warehouses_for_item[warehouse_name] = quantity_remaining
+                    quantity_remaining = 0
                     break
 
-                elif remaining > wh_stock:
-                    tentative_item_distribution[wh_name] = wh_stock
-                    remaining -= wh_stock
+                # If the warehouse has some of `item` but not enough to complete
+                # the order, take all of its item inventory and look in more warehouses
+                elif quantity_remaining > item_stock:
+                    warehouses_for_item[warehouse_name] = item_stock
+                    quantity_remaining -= item_stock
 
-            # If we have enough to fulfill order, lower warehouse stock accordingly
-            # Otherwise, nothing happens and none of the item is shipped
-            if remaining == 0:
-                for wh_name, item_quantity in tentative_item_distribution.items():
 
-                    # If the warehouse isn't shipping anything yet, create its order
-                    if wh_name not in item_distribution:
-                        item_distribution[wh_name] = {}
+            # If we don't have enough of item to fulfill the order, our behaviour
+            # depends on the flag `_SHIP_PARTIAL_ORDERS`. If True, we ship none of this
+            # item and move on to the next. If False, the entire order is cancelled and
+            # we ship no items (even ones that we have enough of!)
+            if quantity_remaining > 0:
+                if cls.shouldShipPartialOrders():
+                    continue
+                else:
+                    return []
 
-                    item_distribution[wh_name][item] = item_quantity
+            # Otherwise, add the item quantities per warehouse to our map of warehouses
+            for warehouse_name, item_quantity in warehouses_for_item.items():
+                item_distribution[warehouse_name][item] = item_quantity
 
-        # Convert our map to a list, as specified in problem description
-        output = []
-        for wh_name, items in item_distribution.items():
-            output.append( { wh_name: items } )
 
+        # Convert our map to a list of one-key maps, as indicated in problem description
+        output = [ { name: items } for name, items in item_distribution.items() ]
         return output
-
--- inventory-allocator/test_InventoryAllocator.py
+++ inventory-allocator/test_InventoryAllocator.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
+# Ben Chapman-Kish - June 2020
+# Deliverr recruiting exercise: inventory InventoryAllocator tests
+# https://github.com/deliverr/recruiting-exercises/tree/master/inventory-InventoryAllocator
 
 import unittest
-from InventoryAllocator import InventoryAllocator as allocator
+from InventoryAllocator import InventoryAllocator
 
 """
 Static unit test class to test InventoryAllocator.ship
@@ -12,19 +15,19 @@ class test_InventoryAllocator(unittest.TestCase):
     Given the warehouse dictionary of an output `warehouse`, returns its name.
     """
     def _get_warehouse_name(self, warehouse):
-        # Assuming well-formed output, the name should be the only key in the dict. 
-        # Save on space and use iterator to get this rather than converting to a list.
+        # Assuming well-formed output, the name should be the only key in the dict
+        # Save on space and use iterator to get this rather than converting to a list
         name = next( iter( warehouse.keys() ) )
         return name
 
     """
-    Searches for a warehouse in `warehouses` with the name `wh_name`.
+    Searches for a warehouse in `warehouses` with the name `warehouse_name`.
     Returns the warehouse object, or None if there is no match.
     """
-    def _find_warehouse_in_list_by_name(self, wh_name, warehouses):
+    def _find_warehouse_in_list_by_name(self, warehouse_name, warehouses):
         for warehouse in warehouses:
             other_name = self._get_warehouse_name(warehouse)
-            if wh_name == other_name:
+            if warehouse_name == other_name:
                 return warehouse
             
         return None
@@ -33,88 +36,258 @@ class test_InventoryAllocator(unittest.TestCase):
     Compares two shipments `expected` and `output`.
     Returns True if the shipments are equivalent, otherwise False.
     """
-    def compare_shipments(self, expected, output):
+    def compare_shipments(self, expected_shipment, output_shipment):
         # Compare lengths of shipments
-        if len(expected) != len(output):
+        if len(expected_shipment) != len(output_shipment):
             return False
 
-        # Compare the warehouses between the two shipments
-        for wh_exp in expected:
-            wh_name = self._get_warehouse_name(wh_exp)
+        # Compare warehouses and quantities between the two shipments
+        for expected_warehouse in expected_shipment:
+            warehouse_name = self._get_warehouse_name(expected_warehouse)
 
-            # Check that expected warehouse exists in output
-            wh_out = self._find_warehouse_in_list_by_name(wh_name, output)
-            if wh_out == None:
+            # Check that expected warehouse exists in the output shipment
+            output_warehouse = \
+                self._find_warehouse_in_list_by_name(warehouse_name, output_shipment)
+            
+            if output_warehouse == None:
                 return False
 
-            # Check that the warehouses contain the same items
-            exp_items = wh_exp[wh_name]
-            out_items = wh_out[wh_name]
-
-            if exp_items.keys() != out_items.keys():
+            # Check that warehouses will ship the same items
+            expected_items = expected_warehouse[warehouse_name]
+            output_items = output_warehouse[warehouse_name]
+            if expected_items.keys() != output_items.keys():
                 return False
             
-            # Compare item inventories
-            for item, exp_inv in exp_items.items():
-                out_inv = out_items[item]
-                if exp_inv != out_inv:
+            # Compare item quantities
+            for item, expected_quantity in expected_items.items():
+                output_quantity = output_items[item]
+                if expected_quantity != output_quantity:
                     return False
 
         # All comparisons are done and the shipments should be equivalent.
         return True
 
+
+
     """
     Tests that InventoryAllocator.ship can produce a well-formed output.
     """
-    def test_output_should_be_well_formed(self):
+    def test_well_formed_output(self):
         order = { "apple": 1 }
-        inventory = [{ "name": "owd", "inventory": { "apple": 1 } }]
-        output = allocator.ship(order, inventory)
+        inventory = [
+            { "name": "owd", "inventory": { "apple": 1 } }
+        ]
+        output = InventoryAllocator.ship(order, inventory)
 
+        # Check that the output is a list, representing the shipment
         self.assertIsInstance(output, list)
-        self.assertEqual(len(output), 1)
 
+        # Check that the shipment's first element is a dict of length 1,
+        # representing a warehouse
+        self.assertTrue(len(output) > 0)
         self.assertIsInstance(output[0], dict)
         self.assertEqual(len(output[0]), 1)
+
+        # Check that the warehouse dict's key is its name, and that its value is a dict,
+        # representing its inventory to be shipped from that warehouse
         self.assertIn("owd", output[0])
-
         self.assertIsInstance(output[0]["owd"], dict)
-        self.assertEqual(len(output[0]["owd"]), 1)
+
+        # Check that the inventory dict's key is an item, and that its value is an int
+        self.assertTrue(len(output[0]["owd"]) > 0)
         self.assertIn("apple", output[0]["owd"])
-
         self.assertIsInstance(output[0]["owd"]["apple"], int)
-        self.assertEqual(output[0]["owd"]["apple"], 1)
 
 
 
+    """
+    Tests that an order for a particular item can be fulfilled if a warehouse has the
+    exact quantity needed.
+    """
     def test_exact_match(self):
-        order = { "apple": 1 }
-        inventory = [{ "name": "owd", "inventory": { "apple": 1 } }]
-        expected = [{ "owd": { "apple": 1 } }]
+        order = { "apple": 3 }
+        inventory = [
+            { "name": "owd", "inventory": { "apple": 3 } }
+        ]
+        expected = [
+            { "owd": { "apple": 3 } }
+        ]
 
-        output = allocator.ship(order, inventory)
+        output = InventoryAllocator.ship(order, inventory)
         self.assertTrue(self.compare_shipments(expected, output))
 
-    def test_insufficient_inventory(self):
-        order = { "apple": 1 }
-        inventory = [{ "name": "owd", "inventory": { "apple": 0 } }]
-        expected = []
+    """
+    Tests that an order for a particular item can be fulfilled if several warehouses
+    has the exact quantity needed.
+    """
+    def test_exact_match_multiple_items(self):
+        order = { "banana": 6, "orange": 2, "apple": 3 }
+        inventory = [
+            { "name": "owd", "inventory": { "apple": 3, "banana": 6 } },
+            { "name": "dm",  "inventory": { "orange": 2 } }
+        ]
+        expected = [
+            { "owd": { "apple": 3, "banana": 6 } },
+            { "dm":  { "orange": 2 } }
+        ]
 
-        output = allocator.ship(order, inventory)
+        output = InventoryAllocator.ship(order, inventory)
         self.assertTrue(self.compare_shipments(expected, output))
 
-    def test_split_across_warehouses(self):
-        order = {"apple": 10}
+    """
+    Tests that an order for a particular item will be fulfilled using the cheapest
+    warehouse even if other ones have enough inventory, and that only the requested
+    quantity will be shipped.
+    """
+    def test_warehouse_precedence(self):
+        order = { "apple": 3 }
         inventory = [
             { "name": "owd", "inventory": { "apple": 5 } },
-            { "name": "dm",  "inventory": { "apple": 5 } }
+            { "name": "dm",  "inventory": { "apple": 3 } },
+            { "name": "pnw", "inventory": { "apple": 6 } },
+            { "name": "lmi", "inventory": { "apple": 1 } }
         ]
-        expected = [{ "dm": { "apple": 5 } }, { "owd": { "apple": 5 } }]
+        expected = [
+            { "owd": { "apple": 3 } }
+        ]
 
-        output = allocator.ship(order, inventory)
+        output = InventoryAllocator.ship(order, inventory)
         self.assertTrue(self.compare_shipments(expected, output))
+
+    """
+    Tests that when an order can be fulfilled for an item but no single warehouse has
+    enough, that the shipment correctly gets the right quantity from each warehouse.
+    """
+    def test_split_across_warehouses(self):
+        order = { "apple": 10 }
+        inventory = [
+            { "name": "owd", "inventory": { "apple": 0 } },
+            { "name": "dm",  "inventory": { "apple": 6 } },
+            { "name": "pnw", "inventory": { "apple": 12 } }
+        ]
+        expected = [
+            { "dm":  { "apple": 6 } },
+            { "pnw": { "apple": 4 } }
+        ]
+
+        output = InventoryAllocator.ship(order, inventory)
+        self.assertTrue(self.compare_shipments(expected, output))
+    
+    """
+    Tests that when an order can be fulfilled for many items but no single warehouse has
+    enough, that the shipment correctly gets the right quantities from each warehouse.
+    """
+    def test_split_multiple_items_across_warehouses(self):
+        order = { "apple": 10, "orange": 1, "banana": 7, "pear": 0 }
+        inventory = [
+            { "name": "owd", "inventory": { "banana": 2 } },
+            { "name": "dm",  "inventory": { "apple": 6, "banana": 0 } },
+            { "name": "pnw", "inventory": { "orange": 20, "apple": 3 } },
+            { "name": "lmi", "inventory": { "apple": 10, "banana": 6 } }
+
+        ]
+        expected = [
+            { "owd": { "banana": 2 } },
+            { "dm":  { "apple": 6 } },
+            { "pnw": { "apple": 3, "orange": 1 } },
+            { "lmi": { "apple": 1, "banana": 5 } }
+        ]
+
+        output = InventoryAllocator.ship(order, inventory)
+        self.assertTrue(self.compare_shipments(expected, output))
+
+    """
+    Tests that when an order can be fulfilled for an item but no warehouse has enough,
+    that the shipment correctly gets the right quantity from each warehouse.
+    """
+    def test_split_across_warehouses(self):
+        order = { "apple": 10 }
+        inventory = [
+            { "name": "owd", "inventory": { "apple": 6 } },
+            { "name": "dm",  "inventory": { "apple": 12 } }
+        ]
+        expected = [
+            { "owd": { "apple": 6 } },
+            { "dm":  { "apple": 4 } }
+        ]
+
+        output = InventoryAllocator.ship(order, inventory)
+        self.assertTrue(self.compare_shipments(expected, output))
+
+    """
+    Tests that an order for a particular item will not be fulfilled if any warehouse
+    that lists it has 0 of it, or if a warehouse doesn't have any inventory.
+    """
+    def test_zero_inventory(self):
+        order = { "apple": 1 }
+        inventory = [
+            { "name": "owd", "inventory": { } },
+            { "name": "pnw", "inventory": { "apple": 0 } }
+        ]
+        expected = []
+
+        output = InventoryAllocator.ship(order, inventory)
+        self.assertTrue(self.compare_shipments(expected, output))
+
+    """
+    Tests that an order for a particular item will be fulfilled with only the warehouses
+    that have it in their inventory, and that no additional warehouses are listed.
+    """
+    def test_zero_inventory_some_warehouses(self):
+        order = { "apple": 3 }
+        inventory = [
+            { "name": "owd", "inventory": { "apple": 0 } },
+            { "name": "dm",  "inventory": { "orange": 5 } },
+            { "name": "pnw", "inventory": { "apple": 3 } },
+            { "name": "lmi", "inventory": { "apple": 1 } }
+        ]
+        expected = [
+            { "pnw": { "apple": 3 } }
+        ]
+
+        output = InventoryAllocator.ship(order, inventory)
+        self.assertTrue(self.compare_shipments(expected, output))
+    
+    """
+    Tests that an order for several items will not be fulfilled if there aren't enough
+    of them across all warehouses to meet the requested quantity.
+    """
+    def test_insufficient_inventory_multiple_items(self):
+        order = { "apple": 4, "orange": 5 }
+        inventory = [
+            { "name": "owd", "inventory": { "apple": 1 } },
+            { "name": "dm",  "inventory": { "apple": 2, "orange": 4 } }
+        ]
+        expected = []
+
+        output = InventoryAllocator.ship(order, inventory)
+        self.assertTrue(self.compare_shipments(expected, output))
+    
+    """
+    Tests the behaviour of the InventoryAllocator when some of the items in the order
+    can be shipped but there is insufficient inventory for others. This behaviour
+    depends on the flag `InventoryAllocator._SHIP_PARTIAL_ORDERS`, and this test has an
+    expected result for either case.
+    """
+    def test_insufficient_inventory_some_items(self):
+        order = { "apple": 5, "orange": 5, "banana": 2 , "pear": 1 }
+        inventory = [
+            { "name": "owd", "inventory": { "apple": 3, "banana": 4 } },
+            { "name": "lmi", "inventory": { "apple": 3, "orange": 4 } }
+        ]
+
+        if InventoryAllocator.shouldShipPartialOrders():
+            expected = [
+                { "owd": { "apple": 3, "banana": 2 } },
+                { "lmi": { "apple": 2 } }
+            ]
+        else:
+            expected = []
+
+        output = InventoryAllocator.ship(order, inventory)
+        self.assertTrue(self.compare_shipments(expected, output))
+
 
 
 if __name__ == '__main__':
     unittest.main()
-
